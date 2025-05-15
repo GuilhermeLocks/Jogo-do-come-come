c91d5f552349855eafc5ba6d6ec9729f4425b1da
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-05-15 20:46:00 -0300

    Update jogo v1.0.py

--- jogo v1.0.py
+++ jogo v1.0.py
@@ -5,7 +5,8 @@ pygame.init()
 
 altura  = 480
 largura = 640
-
+moeda = 0
+valor = 1
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo Clicker')
 
@@ -16,3 +17,24 @@ while True:
             exit()
     pygame.draw.rect(tela, (0,255,0), (200, 300, 40, 50))
     pygame.display.update()
+
+    acao = input('{}'.format(moeda)).upper()
+
+    if acao == '':
+        moeda += valor
+
+    if acao == 'L':
+        compra = input('''
+    [1] +2 moeda=10
+    [2] +5 moeda=1000
+    [3] s para sair''')
+
+    if compra == '1' and moeda >= 10:
+        moeda -= 10
+        valor += 1
+        compra = ''
+
+    if compra == '2' and moeda >= 1000:
+        moeda -= 1000
+        valor += 5
+        compra = ''
